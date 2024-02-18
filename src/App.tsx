a8b8e1a4c2281cae289216fc3bc72522066a6dd4
taisarantes <tais.reis.arantes@gmail.com>
2024-02-18 13:19:09 -0300

    props e hooks react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import Home from './paginas/home/Home';
 import './App.css';
-
+import { useState } from 'react'
 
 function App() {
+  const [valor, setValor] = useState(0)
+
+  function handleClick(){
+    setValor(valor + 1)
+  }
+
   return (
-   <Home />
+  //  <Home title='Componente Home'
+  //        description='Este é um componente Home que recebe props.' />
+
+  <div>
+    <p>O valor é: {valor}</p>
+    <button onClick={handleClick}>Adicionar 1</button>
+  </div>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,20 @@
 import './Home.css';
 
-function Home(){
+interface minhaProps{
+    title: string
+    description: string
+}
+
+function Home(props:minhaProps){
     return (
         <>
-            <h1 className='titulo'>Home</h1>
-            <img src='https://i.imgur.com/H88yIo2.png' alt='Imagem Tela Inicial' className='img'></img>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+
+        {/* <h1 className='titulo'>Home</h1> */}
+        {/* <img src='https://i.imgur.com/H88yIo2.png' alt='Imagem Tela Inicial' className='img'></img> */}
         </>
     );
 }
 
-export default Home;
+export default Home
