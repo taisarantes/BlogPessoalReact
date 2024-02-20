306a4f4c8e9a9b31aabe866244ab64e91565279a
taisarantes <tais.reis.arantes@gmail.com>
2024-02-20 09:12:13 -0300

    flexbox com Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
+import './App.css'
+import Home from './paginas/home/Home'
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
+    <Home/>
     </>
-);
+)
 }
 export default App
