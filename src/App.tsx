e1798d4b6c18c8e92fdc325373b3c8f68e7b932b
taisarantes <tais.reis.arantes@gmail.com>
2024-02-20 14:21:39 -0300

    correção tailwind e adição das rotas para login e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import './App.css';
-import Home from './paginas/home/Home'
-import NavBar from './components/navBar/NavBar'
-import Footer from './components/footer/Footer'
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
-      <Footer />
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  );
+);
 }
 
-export default App;
+export default App
