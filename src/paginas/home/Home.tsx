eab655689d8afc00eace8a90f080f41bf8237bc1
taisarantes <tais.reis.arantes@gmail.com>
2024-02-27 08:53:00 -0300

    adição da página perfil

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,4 @@
-import homeLogo from '../../assets/home.png'
+import homeLogo from '../../assets/home1.png'
 import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
@@ -16,7 +16,7 @@ function Home() {
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              {/* <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button> */}
             </div>
             </div>
   
